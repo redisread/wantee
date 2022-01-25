8ef1a2abbcd6fc1cbb893613451b83559511d1f0
wujiahong06 <wujiahong06@meituan.com>
2022-01-25 15:23:16 +0800

    初始化项目

---
+++ src/main/java/com/jiahongw/wantee/WanteeApplication.java
@@ -0,0 +1,13 @@
+package com.jiahongw.wantee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanteeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WanteeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiahongw/wantee/WanteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiahongw.wantee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanteeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
