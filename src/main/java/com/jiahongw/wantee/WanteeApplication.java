12d5d8618a54308743adcf778dd368c311c564ea
redisread <wujiahong19981022@outlook.com>
2022-01-25 15:42:51 +0800

    暂时取消使用数据库

--- src/main/java/com/jiahongw/wantee/WanteeApplication.java
+++ src/main/java/com/jiahongw/wantee/WanteeApplication.java
@@ -2,8 +2,9 @@ package com.jiahongw.wantee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class WanteeApplication {
 
     public static void main(String[] args) {
